f55a9b088e0ff894a88fc236911954e2bf3ec5ee
Jostein Tollefsrud <mail@josteintollefsrud.no>
2021-04-27 00:35:23 +0200

    add different styling for overdue and today

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import './Card.css';
+import Button from '../button/Button';
+import { useSelector } from 'react-redux';
 
 const Card = ({
     imageSrc = "assets/img/default-plant.jpg", 
@@ -11,7 +13,11 @@ const Card = ({
     _id
 }) => {
 
-    const warning = (fertilizing < 0) || (watering < 0) || (fertilizing === 0) || (watering === 0);
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const warning = (fertilizing === 0) || (watering === 0);
+    const danger = (fertilizing < 0) || (watering < 0);
     const wateringToday = watering === 0;
     const wateringOverdue = watering < 0;
     const fertilizingToday = fertilizing === 0;
@@ -24,15 +30,17 @@ const Card = ({
                 <div className="card__subheader">{subheader}</div>
 
                 {wateringToday && <div className={`${warning && 'warning'}`}>Plant need watering today!</div>}
-                {wateringOverdue && <div className={`${warning && 'warning'}`}>{`Watering is overdue by ${watering} days!`}</div>}
+                {wateringOverdue && <div className={`${danger && 'overdue'}`}>{`Watering is overdue by ${watering} days!`}</div>}
                 {watering > 0 && <div>{`Next watering in ${watering} days`}</div>}
 
                 {fertilizingToday && <div className={`${warning && 'warning'}`}>Plant need fertilizing today!</div>}
-                {fertilizingOverdue && <div className={`${warning && 'warning'}`}>{`Fertilizing is overdue by ${fertilizing} days!`}</div>}
+                {fertilizingOverdue && <div className={`${danger && 'overdue'}`}>{`Fertilizing is overdue by ${fertilizing} days!`}</div>}
                 {fertilizing > 0 && <div>{`Next fertilizing in ${fertilizing} days`}</div>}
 
-                <Link className="card__link" to={`plants/${_id}`}>View details</Link>
-                <button className="card__button">Water</button>
+                <div className="card__actions">
+                    <Link className="card__link" to={`plants/${_id}`}>View details</Link>
+                    {userInfo && <Button className="card__button" size="btn--small" value="Water" />}
+                </div>
             </div>
             <img className="card__image" src={imageSrc} alt={altText} />
         </div>
