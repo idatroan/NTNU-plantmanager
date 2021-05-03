f8d2eb7cf9dd776748ed3629fb49f600ce5446af
Jostein Tollefsrud <mail@josteintollefsrud.no>
2021-05-03 22:59:00 +0200

    add onclick functionalities on new button

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -1,13 +1,15 @@
 import './Button.css';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
-const Button = ({ type, value, loading=false, variant="primary" }) => {
+const Button = ({onClick, type, value, loading=false, variant="primary" }) => {
 
     return ( 
         <button 
+            onClick={onClick}
             className={`button ${variant}`} 
-            type={type}>{value} 
-            {loading && <LoadingSpinner size="small" color="light"/>}
+            type={type}>
+                {value} 
+                {loading && <LoadingSpinner size="small" color="light"/>}
         </button>
     );
     
