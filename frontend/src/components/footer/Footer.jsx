d8278890ebc2e36e2bec13fb2f74b3a5e45e4554
Jostein Tollefsrud <mail@josteintollefsrud.no>
2021-05-05 18:00:26 +0200

    add ragni to footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
                 In this project we were meant to make a basic user and plant system, where you can log in,
                 have a role that allows you to water or fertilize the plants. These plants will be listed
                 at the front page where you can sort through which needs watering or fertilizing first.
-                etc.......
+                <hr/>
 
-                Coded by Ida Therese Hongset Trøan and Jostein Tollefsrud
+                Coded by Ida Therese Hongset Trøan, Jostein Tollefsrud and Ragni Støen
             </p>
             <img src={process.env.PUBLIC_URL + '/assets/img/ntnu-logo.png'} alt="" />
         </footer>
