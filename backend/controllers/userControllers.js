75cd25814c4e0a36eba517d775d7d16da11a5e4c
jostein-tollefsrud <mail@josteintollefsrud.no>
2021-06-09 13:19:52 +0200

    secured update

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,3 @@
-
 const User = require('../models/user');
 const utils = require('../lib/utils');
 
@@ -25,7 +24,8 @@ const getUserById = async (req, res) => {
         if (manager) return res.status(200).json(userToFind);
 
         // Users can only view their own profile
-        if (userToFind.id !== userLoggedIn) return res.status(401).json({ message: 'Unauthorized' })
+        if (userToFind.id !== userLoggedIn)
+            return res.status(401).json({ message: 'Unauthorized' });
         res.status(200).json(userToFind);
     } catch (error) {
         res.status(500).json({ message: error });
@@ -36,7 +36,7 @@ const getUserById = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error });
     }*/
-}
+};
 
 /**
  * -------------- GET /users --------------
@@ -48,13 +48,13 @@ const getUserById = async (req, res) => {
  */
 const getAllUsers = async (req, res) => {
     const users = await User.find().select('-password');
-    
+
     try {
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ message: error });
     }
-}
+};
 
 /**
  * -------------- PUT /users/:id --------------
@@ -65,13 +65,18 @@ const getAllUsers = async (req, res) => {
  * @param {Object} res - Use to send back the desired HTTP response
  */
 const updateUserById = async (req, res) => {
-
     try {
         // Managers can edit all user field exept password
         const manager = req.user.role === 'manager';
 
         const user = await User.findById(req.params.id);
         const { firstName, lastName, role, email } = req.body;
+        const emailInDb = await User.findOne({ email: req.body.email });
+
+        if (emailInDb) return res.status(400).json('Email is taken!');
+
+        // Må finne en løsning slik at en bruker kan bruke opp igjen sin gamle email
+        // if(req.user.id === user._id)
 
         if (manager) {
             user.firstName = firstName;
@@ -82,17 +87,22 @@ const updateUserById = async (req, res) => {
             await user.save();
             res.status(200).json(`${user.firstName} have been updated!`);
         } else {
+            const userId = req.user.id;
+            if (userId !== req.params.id) {
+                return res
+                    .status(400)
+                    .json('You can only update your own profile!');
+            }
             user.firstName = firstName;
             user.lastName = lastName;
 
             await user.save();
             res.status(200).json(`${user.firstName} have been updated!`);
         }
-
     } catch (error) {
         res.status(500).json({ message: error });
     }
-}
+};
 
 /**
  * -------------- DELETE /users/:id --------------
@@ -104,17 +114,25 @@ const updateUserById = async (req, res) => {
  */
 const deleteUserById = async (req, res) => {
     const user = await User.findById(req.params.id);
+
+    // Only managers can delete a user
+    const manager = req.user.role === 'manager';
+    if (!manager)
+        return res.status(400).json('You are not authorized to delete!');
+
     try {
         user.remove();
-        res.status(200).json({ message: `${user.firstName} ${user.lastName} has been deleted!` })
+        res.status(200).json({
+            message: `${user.firstName} ${user.lastName} has been deleted!`,
+        });
     } catch (error) {
         res.status(500).json({ message: error });
     }
-}
+};
 
 module.exports = {
     getUserById,
     getAllUsers,
     updateUserById,
-    deleteUserById
+    deleteUserById,
 };
